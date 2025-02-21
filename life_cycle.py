c7f2da1c1a9f4a0210e1c8425b807e05317b1167
Julian Currie <75761829+jccurrie@users.noreply.github.com>
2025-02-21 15:56:33 -0600

    Refactored and cleaned up, Docstrings added, Formatted

--- life_cycle.py
+++ life_cycle.py
@@ -1,130 +1,86 @@
-from datetime import datetime
-from datetime import timedelta
+from datetime import datetime, timedelta
 import pandas as pd
 import matplotlib.pyplot as plt
 
-'''C.Elegans life stages'''
+# C. Elegans Life Stages
 STAGES = ["EGG", "L1", "L2", "L3", "L4", "ADULT"]
 
-'''String format for time'''
-TIME_FRAME = '%m %d %Y %I:%M %p'
+# Date-Time Format
+TIME_FORMAT = "%m/%d/%Y %I:%M %p"
 
-'''Dictionary of worm Stages and Times at different storage temperatures 25Celcius,20Celcius,15Celcius'''
-WORM = {
-        'Temp' : {
-                '25Celcius' : {
-                        'Stage' : STAGES,
-                        'Time' : []
-                        },
-                '20Celcius' : {
-                        'Stage' : STAGES,
-                        'Time' : []
-                        },
-                '15Celcius' : {
-                        'Stage' : STAGES,
-                        'Time' : []
-                        }
-                }
-        }
+# Worm Stages and Times at Different Storage Temperatures
+WORM_LIFE_CYCLE = {
+    "25C": {"Stages": STAGES, "Times": []},
+    "20C": {"Stages": STAGES, "Times": []},
+    "15C": {"Stages": STAGES, "Times": []},
+}
+
+# Time intervals for different temperatures (in hours)
+TIME_INTERVALS = {
+    "L1": {"25C": 8, "20C": 10.4, "15C": 16.8},
+    "L2": {"25C": 20, "20C": 26, "15C": 42},
+    "L3": {"25C": 27, "20C": 35.1, "15C": 56.7},
+    "L4": {"25C": 34, "20C": 44.2, "15C": 71.4},
+    "ADULT": {"25C": 43, "20C": 55.9, "15C": 90.3},
+}
+
+
+def get_user_input():
+    """Prompts user for the starting date/time and returns a formatted datetime object."""
+    date_str = input("Enter the starting date (MM/DD/YYYY HH:MM AM/PM): ")
+    return datetime.strptime(date_str, TIME_FORMAT)
+
+
+def calculate_life_cycle(start_time):
+    """Calculates the life cycle times based on storage temperature and stage."""
+    for temp in WORM_LIFE_CYCLE:
+        WORM_LIFE_CYCLE[temp]["Times"].append(start_time.strftime(TIME_FORMAT))
+
+    for stage in STAGES[1:]:  # Skip 'EGG' as it's the starting point
+        for temp in WORM_LIFE_CYCLE:
+            new_time = start_time + timedelta(hours=TIME_INTERVALS[stage][temp])
+            WORM_LIFE_CYCLE[temp]["Times"].append(new_time.strftime(TIME_FORMAT))
+
+
+def display_life_cycle():
+    """Prints the calculated life cycle times for each temperature."""
+    for temp, data in WORM_LIFE_CYCLE.items():
+        print(f"\n=== Life Cycle at {temp} ===")
+        for stage, time in zip(data["Stages"], data["Times"]):
+            print(f"Stage: {stage.ljust(5)} - Time: {time}")
+
+
+def plot_life_cycle():
+    """Generates a line plot of the life cycle stages at different temperatures."""
+    df = pd.DataFrame(WORM_LIFE_CYCLE)
+    fig, axes = plt.subplots(3, figsize=(12, 9), sharex=True)
+    fig.suptitle("C. Elegans Life Cycle Stages")
+
+    colors = {"25C": "b", "20C": "r", "15C": "g"}
+    for ax, temp in zip(axes, WORM_LIFE_CYCLE.keys()):
+        ax.plot(
+            df[temp]["Stages"],
+            df[temp]["Times"],
+            color=colors[temp],
+            marker="o",
+            label=f"{temp}",
+        )
+        ax.legend(loc="upper left")
+        ax.set_ylabel("Time")
+
+    plt.xlabel("Life Stages")
+    plt.xticks(rotation=45)
+    plt.tight_layout(rect=[0, 0, 1, 0.96])
+    plt.show()
+
 
 def main():
-        starting_day = getUserInput()
-        calculate_cycle(starting_day, STAGES)
-        printWorm()
-        print("\n**************************\n")
-        gen_dataframe()
+    start_time = get_user_input()
+    print(f"\nCycle Start Date: {start_time.strftime('%B %d, %Y %I:%M %p')}")
+    calculate_life_cycle(start_time)
+    display_life_cycle()
+    plot_life_cycle()
 
-'''Prompts user for input and returns string'''
-def getUserInput():
-        print("\n")
-        starting_day = input("What is your starting Date? ( in MM DD YYYY HH:mm ): ")
-        return starting_day
-
-'''Caclulates life cycle based on storage temperature and stage.'''
-def calculate_cycle(starting_day, STAGES):
-
-        starting_day_ref = datetime.strptime(starting_day,"%m %d %Y %I:%M")
-        print("Your cycle start date is : "+starting_day_ref.strftime('%B %d %Y %I:%M'))
-
-        for stage in STAGES:
-                if stage == "EGG":
-                        WORM['Temp']['25Celcius']['Time'].append((starting_day_ref).strftime(TIME_FRAME))
-                        WORM['Temp']['20Celcius']['Time'].append((starting_day_ref).strftime(TIME_FRAME))
-                        WORM['Temp']['15Celcius']['Time'].append((starting_day_ref).strftime(TIME_FRAME))
-
-                elif stage == "L1":
-                        new_time25 = (starting_day_ref + timedelta(hours=8))
-                        new_time20 = (starting_day_ref + timedelta(hours=10.4))
-                        new_time15 = (starting_day_ref + timedelta(hours=16.8))
-
-                        WORM['Temp']['25Celcius']['Time'].append(str(new_time25.strftime(TIME_FRAME)))
-                        WORM['Temp']['20Celcius']['Time'].append(str(new_time20.strftime(TIME_FRAME)))
-                        WORM['Temp']['15Celcius']['Time'].append(str(new_time15.strftime(TIME_FRAME)))           
-
-                elif stage == "L2":
-                        new_time25 = (starting_day_ref + timedelta(hours=20))
-                        new_time20 = (starting_day_ref + timedelta(hours=26))
-                        new_time15 = (starting_day_ref + timedelta(hours=42))
-
-                        WORM['Temp']['25Celcius']['Time'].append(str(new_time25.strftime(TIME_FRAME)))
-                        WORM['Temp']['20Celcius']['Time'].append(str(new_time20.strftime(TIME_FRAME)))
-                        WORM['Temp']['15Celcius']['Time'].append(str(new_time15.strftime(TIME_FRAME)))       
-
-                elif stage == "L3":
-                        new_time25 = (starting_day_ref + timedelta(hours=27))
-                        new_time20 = (starting_day_ref + timedelta(hours=35.1))
-                        new_time15 = (starting_day_ref + timedelta(hours=56.7))
-
-                        WORM['Temp']['25Celcius']['Time'].append(str(new_time25.strftime(TIME_FRAME)))
-                        WORM['Temp']['20Celcius']['Time'].append(str(new_time20.strftime(TIME_FRAME)))
-                        WORM['Temp']['15Celcius']['Time'].append(str(new_time15.strftime(TIME_FRAME)))       
-                        
-                elif stage == "L4":
-                        new_time25 = (starting_day_ref + timedelta(hours=34))
-                        new_time20 = (starting_day_ref + timedelta(hours=44.2))
-                        new_time15 = (starting_day_ref + timedelta(hours=71.4))
-
-                        WORM['Temp']['25Celcius']['Time'].append(str(new_time25.strftime(TIME_FRAME)))
-                        WORM['Temp']['20Celcius']['Time'].append(str(new_time20.strftime(TIME_FRAME)))
-                        WORM['Temp']['15Celcius']['Time'].append(str(new_time15.strftime(TIME_FRAME)))       
-
-                elif stage == "ADULT":
-                        new_time25 = (starting_day_ref + timedelta(hours=43))
-                        new_time20 = (starting_day_ref + timedelta(hours=55.9))
-                        new_time15 = (starting_day_ref + timedelta(hours=90.3))
-
-                        WORM['Temp']['25Celcius']['Time'].append(str(new_time25.strftime(TIME_FRAME)))
-                        WORM['Temp']['20Celcius']['Time'].append(str(new_time20.strftime(TIME_FRAME)))
-                        WORM['Temp']['15Celcius']['Time'].append(str(new_time15.strftime(TIME_FRAME)))       
-
-def printWorm():
-        # For temp in worm
-        for temp in WORM['Temp']:
-                print("\n**************************\n")
-                print("At STORAGE TEMP: ", temp)
-                # for stage in temp
-                for stage_time in WORM['Temp'][temp]:
-                        # for each index in stage and time in temp
-                        for i in range(len(WORM['Temp'][temp]['Stage'])):
-                                if stage_time == "Stage":
-                                        print("STAGE: ", str(WORM['Temp'][temp]['Stage'][i]).ljust(5), "= TIME: ", str(WORM['Temp'][temp]['Time'][i]).ljust(5))
-                                else:
-                                        break
-
-def gen_dataframe():
-        df = pd.DataFrame.from_dict(WORM)
-        fig, (ax1, ax2, ax3) = plt.subplots(3,figsize=(12,9))
-        fig.suptitle('C.Elegans Life Cycle Calculation')
-        ax1.plot(df['Temp']['25Celcius']['Stage'],df['Temp']['25Celcius']['Time'], c='b', marker='o', label='25 Celcius')
-        ax2.plot(df['Temp']['20Celcius']['Stage'],df['Temp']['20Celcius']['Time'], c='r', marker='o', label='20 Celcius')
-        ax3.plot(df['Temp']['15Celcius']['Stage'],df['Temp']['15Celcius']['Time'], c='g', marker='o', label='15 Celcius')
-        ax1.legend(loc='upper left')
-        ax2.legend(loc='upper left')
-        ax3.legend(loc='upper left')
-        
-        plt.show()
 
 if __name__ == "__main__":
-        main()
-
-
+    main()
